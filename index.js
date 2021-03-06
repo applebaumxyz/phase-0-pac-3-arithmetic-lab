0bf907321f7f15ea578f7dc1369713b9681ed42d
+ <+>
2021-03-06 01:46:59 -0500

    Complete assignment

---
+++ index.js
@@ -0,0 +1,82 @@
+
+function myFunction() {
+    console.log()
+    return
+}
+
+function add() {
+    console.log
+    return
+}
+
+
+function subtract() {
+    console.log()
+    return
+}
+
+
+
+function multiply() {
+    console.log()
+    return
+}
+
+
+
+function divide() {
+    console.log()
+    return
+}
+
+let number = 10
+
+function add5() {
+    return number +=5
+}
+
+function divideBy3() {
+    return number /= 3
+}
+
+function increment(number) {
+    return number +=1
+}
+
+function decrement(number) {
+    return number -= 1
+}
+
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function myFunction() {
+    return
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
+
+
+
+
